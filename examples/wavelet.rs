a160a96271290922ad40c13e8f25cf7eba30dc40
msiglreith <msiglreith@users.noreply.github.com>
2017-02-20 23:42:22 +0100

    Implement a generic wavelet trait

--- examples/wavelet.rs
+++ examples/wavelet.rs
@@ -4,35 +4,62 @@ extern crate cgmath;
 extern crate ndarray;
 extern crate image;
 extern crate panopaea_util as util;
+extern crate generic_array;
 
 use image::GenericImage;
 
 use cgmath::BaseFloat;
 use ndarray::{arr1, ArrayView, ArrayViewMut, Array1, Array2, Axis, Ix1};
+use generic_array::ArrayLength;
+use generic_array::typenum::{U2};
 
-// Haar reconstruction
-// low-pass:  0.7071067811865476 0.7071067811865476
-// high-pass: 0.7071067811865476 0.7071067811865476
+pub trait Wavelet<T> {
+    type N: ArrayLength<usize>;
+    fn coeff_up_low() -> &'static [T];
+    fn coeff_up_high() -> &'static [T];
+    fn coeff_down_low() -> &'static [T];
+    fn coeff_down_high() -> &'static [T];
+}
+
+// Haar wavelet
+static HAAR_UP_LOW:  &[f64; 2] = &[0.7071067811865476,  0.7071067811865476];
+static HAAR_UP_HIGH: &[f64; 2] = &[0.7071067811865476, -0.7071067811865476];
+static HAAR_DOWN_LOW : &[f64; 2] = &[ 0.7071067811865476, 0.7071067811865476];
+static HAAR_DOWN_HIGH: &[f64; 2] = &[-0.7071067811865476, 0.7071067811865476];
+
+pub struct Haar;
+impl Wavelet<f64> for Haar {
+    type N = U2;
+    #[inline]
+    fn coeff_up_low() -> &'static [f64] { HAAR_UP_LOW }
+    #[inline]
+    fn coeff_up_high() -> &'static [f64] { HAAR_UP_HIGH }
+    #[inline]
+    fn coeff_down_low() -> &'static [f64] { HAAR_DOWN_LOW }
+    #[inline]
+    fn coeff_down_high() -> &'static [f64] { HAAR_DOWN_HIGH }
+}
+
 
 #[inline]
 fn down_convolution(input: ArrayView<f64, Ix1>, mut output: ArrayViewMut<f64, Ix1>) {
     let half_size = output.len() / 2;
 
-    // TODO: generalize to all wavelets
-    // TODO: non-pow2 arrays
+    let low_pass = Haar::coeff_down_low();
+    let high_pass = Haar::coeff_down_high();
 
     // low-pass
     for i in 0..half_size {
         output[i] =
-            input[2*i  ] * 0.7071067811865476 +
-            input[2*i+1] * 0.7071067811865476;
+            input[2*i  ] * low_pass[0] +
+            input[2*i+1] * low_pass[1];
     }
 
     // high pass
     for i in 0..half_size {
         output[i+half_size] =
-           -input[2*i  ] * 0.7071067811865476 +
-            input[2*i+1] * 0.7071067811865476;
+            input[2*i  ] * high_pass[0] +
+            input[2*i+1] * high_pass[1];
     }
 }
 
@@ -83,19 +110,19 @@ fn main() {
     {
         let lena = image::open("examples/data/lena.png").unwrap().flipv();
         let mut output = Array2::from_elem((lena.height() as usize, lena.width() as usize), 0.0);
+
         for y in 0..lena.height() {
             let row = (0..lena.width()).into_iter().map(|x| {
                 lena.get_pixel(x, y).data[0] as f64
             }).collect::<Vec<_>>();
-
             let input = arr1(&row);
 
             down_convolution(input.view(), output.subview_mut(Axis(0), y as usize));
         }
 
-        for x in 0..lena.width() as usize {
-            let col = (0..lena.height() as usize).into_iter().map(|y| {
-                output[(y, x)]
+        for x in 0..lena.width() {
+            let col = (0..lena.height()).into_iter().map(|y| {
+                output[(y as usize, x as usize)]
             }).collect::<Vec<_>>();
             let input = arr1(&col);
 
